2da40cf3b6558eeec4c91d7bc800fec45a19770f
Kanishk Gupta <91678929+kanishk5268@users.noreply.github.com>
2024-03-24 11:25:53 +0530

    added very simple pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,28 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const Article = require('./../models/article.model');
+const Article = require("./../models/article.model");
 //routes
 
-router.get("/",async (req, res) => {
-   const locals = {
-    title: "BlogApp",
-    description:"It is simple Blog App"
-   }
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "BlogApp",
+      description: "It is simple Blog App",
+    };
 
+    let perPage = 10;
+    let page = req.query.page || 1;
 
-   try {
-    const data = await Article.find();
-    res.render("index",{locals,data});
-   } catch (error) {
+    const data = await Article.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count =   await Article.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
     console.log(error);
-   }
-  });
-
-  
-
-
-
-
+  }
+});
 
 router.get("/about", (req, res) => {
   res.render("about");
